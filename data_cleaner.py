94c07f73312b9e02b7e2ea896bf3f2983c937cd6
Samantha Kyle <samanthalkyle@gmail.com>
2026-02-10 15:56:54 -0500

    adding data cleaning file

---
+++ data_cleaner.py
@@ -0,0 +1,17 @@
+import geopandas as gpd
+
+# read in the file
+full_file = gpd.read_file('./data/crime.geojson')
+
+# separate by datatype and rename for consistency
+just_robbery = full_file[['ROBBERY_RATE_2025', 'geometry']]
+just_robbery.rename(columns = {'ROBBERY_RATE_2025': 'DATA'}, inplace=True)
+just_theft = full_file[['AUTOTHEFT_RATE_2025', 'geometry']]
+just_theft.rename(columns = {'AUTOTHEFT_RATE_2025': 'DATA'}, inplace=True)
+just_assault = full_file[['ASSAULT_RATE_2025', 'geometry']]
+just_assault.rename(columns = {'ASSAULT_RATE_2025': 'DATA'}, inplace=True)
+
+# write to clean small files
+just_robbery.to_file('robbery.geojson', driver = 'GeoJSON')
+just_theft.to_file('theft.geojson', driver = 'GeoJSON')
+just_assault.to_file('assualt.geojson', driver='GeoJSON')
