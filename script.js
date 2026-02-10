8a7b24763bee7d476f2220b60249be3bf82e1cd9
Samantha Kyle <samanthalkyle@gmail.com>
2026-02-10 15:39:11 -0500

    fixed before submission

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
+// token to use mapbox
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2FtLWt5bGUiLCJhIjoiY21rZTR3NW82MDNjazNscHdvZGRoNTJlYyJ9.gXrPIIVvGXk6SEwdzrbd1g'
 
+// map object to insert and interact with
 const map = new mapboxgl.Map({
+    // the container id where it will be created
     container: 'my-map',
 
+    // style obtained from my custom mapbox style
     style: 'mapbox://styles/sam-kyle/cmlfuds0e003w01s4hs6i6aa2',
+
+    // starting center point for initial load in 
     center: [-79.294541, 43.67431],
 
-    // starting position [lng, lat]
-    zoom: 10, // starting zoom
+    zoom: 9, // starting zoom
 })
+
+// got quartile values for each dataset (in excel) and placed them here for easy calling
+const robbery_quartiles = [0, 44.25, 69.46, 96.22];
+const assault_quartiles = [0, 466.73, 636.31, 866.48];
+const autotheft_quartiles = [0, 122.99, 192.6, 266.14];
+
+// set my color scale here to make it easy to edit later
+const colors = ['#d64626', '#d87c42', '#e6aa61', '#F2F12D']
+
+
 
 map.on('load', () => {
     // this is the eventListener
-    // Add a data source containing GeoJSON data
 
-    // must add data sources here
+    // must add data sources here and subsequent layers
 
     map.addSource('police', {
         'type': 'geojson',
@@ -42,38 +56,31 @@ map.on('load', () => {
         'data': 'https://raw.githubusercontent.com/SamanthaKyle/Lab2_final/refs/heads/main/data/crime.geojson'
     }
     );
-    // Add a layer showing the places.
+    // Add a layer showing the assault
     map.addLayer({
         'id': 'assault',
         'type': 'fill',
         'slot': 'middle',
         'source': 'assault',
+        'layout': {
+            'visibility': 'none'
+        },
         'paint': {
-                'fill-color': [
-                    'interpolate',
-                    ['linear'],
-                    ['get', 'ASSAULT_RATE_2025'],
-                    0,
-                    '#F2F12D',
-                    330,
-                    '#EED322',
-                    660,
-                    '#E6B71E',
-                    990,
-                    '#DA9C20',
-                    1320,
-                    '#CA8323',
-                    1650,
-                    '#B86B25',
-                    1980,
-                    '#A25626',
-                    2310,
-                    '#8B4225',
-                    4400,
-                    '#723122'
-                ],
-                'fill-opacity': 0.9
-            
+            'fill-color': [
+                'interpolate',
+                ['linear'],
+                ['get', 'ASSAULT_RATE_2025'],
+                assault_quartiles[0],
+                colors[3],
+                assault_quartiles[1],
+                colors[2],
+                assault_quartiles[2],
+                colors[1],
+                assault_quartiles[3],
+                colors[0]
+            ],
+            'fill-opacity': 0.9
+
         }
     });
 
@@ -83,47 +90,75 @@ map.on('load', () => {
         'data': 'https://raw.githubusercontent.com/SamanthaKyle/Lab2_final/refs/heads/main/data/crime.geojson'
     }
     );
-    // Add a layer showing the places.
-    // map.addLayer({
-    //     'id': 'robbery',
-    //     'type': 'fill',
-    //     'slot': 'middle',
-    //     'source': 'robbery',
-    //     'paint': {
-    //         'fill-color': [
-    //             'interpolate',
-    //             ['linear'],
-    //             ['get', 'ROBBERY_RATE_2025'],
-    //             0,
-    //             '#F2F12D',
-    //             330,
-    //             '#EED322',
-    //             660,
-    //             '#E6B71E',
-    //             990,
-    //             '#DA9C20',
-    //             1320,
-    //             '#CA8323',
-    //             1650,
-    //             '#B86B25',
-    //             1980,
-    //             '#A25626',
-    //             2310,
-    //             '#8B4225',
-    //             4400,
-    //             '#723122'
-    //         ],
-    //         'fill-opacity': 0.9
+    // Add a layer showing the robbery rates
+    map.addLayer({
+        'id': 'robbery',
+        'type': 'fill',
+        'slot': 'middle',
+        'source': 'robbery',
+        'layout': {
+            'visibility': 'none'
+        },
+        'paint': {
+            'fill-color': [
+                'interpolate',
+                ['linear'],
+                ['get', 'ROBBERY_RATE_2025'],
+                robbery_quartiles[0],
+                colors[3],
+                robbery_quartiles[1],
+                colors[2],
+                robbery_quartiles[2],
+                colors[1],
+                robbery_quartiles[3],
+                colors[0]
+            ],
+            'fill-opacity': 0.9
 
-    //     },
-    //     'visibile': 'none'
-    // });
+        },
+    });
 
+    map.addSource('theft', {
+        'type': 'geojson',
+        'generateId': true,
+        'data': 'https://raw.githubusercontent.com/SamanthaKyle/Lab2_final/refs/heads/main/data/crime.geojson'
+    }
+    );
+    // Add a layer showing the autotheft rates
+    map.addLayer({
+        'id': 'theft',
+        'type': 'fill',
+        'slot': 'middle',
+        'source': 'theft',
+        'layout': {
+            'visibility': 'none'
+        },
+        'paint': {
+            //'fill-opacity': 0.9,
+            'fill-color': [
+                'interpolate',
+                ['linear'],
+                ['get', 'AUTOTHEFT_RATE_2025'],
+                autotheft_quartiles[0],
+                colors[3],
+                autotheft_quartiles[1],
+                colors[2],
+                autotheft_quartiles[2],
+                colors[1],
+                autotheft_quartiles[3],
+                colors[0]
+            ],
+
+        },
+    });
+
+    // popup object to let us see police stations
     const popup = new mapboxgl.Popup({
         closeButton: false,
         closeOnClick: false
     });
 
+    // show the popup when mouse enters it
     map.addInteraction('police-mouseenter-interaction', {
         type: 'mouseenter',
         target: { layerId: 'police' },
@@ -139,6 +174,7 @@ map.on('load', () => {
         }
     });
 
+    // remove popup when mouse leaves
     map.addInteraction('police-mouseleave-interaction', {
         type: 'mouseleave',
         target: { layerId: 'police' },
@@ -147,19 +183,53 @@ map.on('load', () => {
             popup.remove();
         }
     });
-    
+
 });
 
-
+// my buttons
 const theft_button = document.getElementById('theft-click');
 theft_button.addEventListener('click', () => {
-    //alert('clicked theft button');
+    /*
+    I know I should be doing this by having one variable like 'active_layer' that styles the active layer 
+    and corresponding button one way and let the others fall to default
+
+    And when a button is clicked I would just change the active-layer variable
+
+    I just couldn't figure this out myself in time for submission
+    */
+    // when this button is pressed, ensure only the right layer is visible and others are not
+    map.setLayoutProperty('assault', 'visibility', 'none');
+    map.setLayoutProperty('robbery', 'visibility', 'none');
+    map.setLayoutProperty('theft', 'visibility', 'visible');
+
+    // similarly set the button style so they can know what data they are viewing
+    theft_button.style.backgroundColor = 'white'
+    robbery_button.style.backgroundColor = 'burlywood';
+    assault_button.style.backgroundColor = 'burlywood';
 })
 
+// analagous to above
 const robbery_button = document.getElementById('robbery-click');
 robbery_button.addEventListener('click', () => {
-    alert('clicked robbery button');
-    map.setLayoutProperty('robbery', 'visibility', 'visible')
+    map.setLayoutProperty('assault', 'visibility', 'none');
+    map.setLayoutProperty('theft', 'visibility', 'none');
+    map.setLayoutProperty('robbery', 'visibility', 'visible');
+    robbery_button.setAttribute('fill-color', 'blue')
+    robbery_button.style.backgroundColor = 'white'
+    theft_button.style.backgroundColor = 'burlywood';
+    assault_button.style.backgroundColor = 'burlywood';
+})
+
+// analagous to above
+const assault_button = document.getElementById('assault-click');
+assault_button.addEventListener('click', () => {
+    map.setLayoutProperty('assault', 'visibility', 'visible');
+    map.setLayoutProperty('theft', 'visibility', 'none');
+    map.setLayoutProperty('robbery', 'visibility', 'none');
+    assault_button.style.backgroundColor = 'white'
+    robbery_button.style.backgroundColor = 'burlywood';
+    theft_button.style.backgroundColor = 'burlywood';
+    
 })
 
 
